34e7c4517aafec1cf871d1fd459ed4dc7422b514
simpson <simpsonqoo@yahoo.com.tw>
2018-07-09 21:23:01 +0800

    modify init and weight

--- NN.hpp
+++ NN.hpp
@@ -1,7 +1,9 @@
 #include <iostream>
-#include <vector>
 #include <random>
 #include <iomanip>
+#include "ea.hpp"
+#include "snake.hpp"
+
 
 class NN
 {
@@ -12,14 +14,22 @@ class NN
 	 * use delete_w(layer, input_neu, output_neu) will make the corresponding weight zero
 	 */
   public:
-	NN(uint32_t layer, uint32_t neu): weight(layer - 2, std::vector<std::vector<double>>(neu, std::vector<double>(neu))), 
-		neuron(layer - 1, std::vector<double>(neu)), out_neuron(3), out_weight(3, std::vector<double>(neu)){}
+	NN(uint32_t layer, uint32_t neu, std::vector<std::vector<std::vector<float>>> w): 
+		weight(w), 
+		neuron(layer - 1, std::vector<float>(neu)), out_neuron(6){}
 
 	// initialize, you should input the input neuron
-	void init(std::vector<double> input_neuron)
+	void init(Snake::Info info)
 	{
-		neuron[0] = std::move(input_neuron);
+		neuron[0][0] = info.food_pos.first;
+		neuron[0][1] = info.food_pos.second;
+		neuron[0][2] = info.distance[0];
+		neuron[0][3] = info.distance[1];
+		neuron[0][4] = info.distance[2];
+		neuron[0][5] = info.distance[3];
+		//neuron[0] = std::move(input_neuron);
 		
+		/*
 		std::random_device rd;
 		std::mt19937 gen(rd());
 		std::uniform_real_distribution<> dis(-1.0, 1.0);
@@ -35,7 +45,7 @@ class NN
 
 		for(auto &i : out_weight)
 			for(auto &j : i)
-				j = dis(gen);
+				j = dis(gen);*/
 	}
 
 	void forward()
@@ -48,10 +58,11 @@ class NN
 			}
 
 		for(uint32_t i = 0; i != out_neuron.size(); ++i)
-			out_neuron[i] = dot(neuron[neuron.size() - 1], out_weight[i]);
+			out_neuron[i] = dot(neuron[neuron.size() - 1], weight[neuron.size() - 1][i]);
 	}
 
 
+	/*
 	//delete weight[layer][input_neu][output_neu] 
 	void delete_w(uint32_t layer, uint32_t input_neu, uint32_t output_neu)
 	{
@@ -61,7 +72,7 @@ class NN
 
 		else
 			weight[layer][output_neu][input_neu] = 0;
-	}
+	}*/
 
 	friend std::ostream& operator << (std::ostream& os, NN n)
 	{
@@ -94,29 +105,29 @@ class NN
 				os << std::endl;
 			}
 		}
-
+/*
 		os << "layer" << n.weight.size() << std::endl;
 		for(auto &i : n.out_weight)
 		{
 			for(auto &j : i)
 				os << std::setw(10) << j << "\t";
 			os << std::endl;
-		}
+		}*/
 		return os;
 	}
 
-	std::vector<std::vector<std::vector<double>>> weight;
-	std::vector<std::vector<double>> neuron;
-	std::vector<double> out_neuron;
-	std::vector<std::vector<double>> out_weight;
+	std::vector<std::vector<std::vector<float>>> weight;
+	std::vector<std::vector<float>> neuron;
+	std::vector<float> out_neuron;
+	std::vector<std::vector<float>> out_weight;
 
 	
-	double dot(std::vector<double> v1, std::vector<double> v2)
+	float dot(std::vector<float> v1, std::vector<float> v2)
 	{
 		if(v1.size() != v2.size())
 			std::cerr << "two vectors are different size!" << std::endl;
 
-		double dot_sum = 0.0;
+		float dot_sum = 0.0;
 		for(uint32_t i = 0; i != v1.size(); ++i)
 			dot_sum += v1[i] * v2[i];
 
